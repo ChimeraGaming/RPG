c38ce252f9713481d602803d2dd87d94ce0bc844
cbenjudah <97Turok@Gmail.com>
2021-06-25 22:01:03 -0800

    Added Test File (Remove at end)
    Added Test File (Remove at end)

---
+++ test.py
@@ -0,0 +1,14 @@
+class Sword:
+    # A simple class
+    # attribute
+    attr1 = 'Wooden'
+    attr2 = 'Iron'
+    attr3 = 'Mythril'
+
+# Driver code
+# Object instantiation
+Sword_1 = Sword()
+
+# Accessing class attributes
+# and method through objects
+print (f'{Sword_1.attr1} Sword')
