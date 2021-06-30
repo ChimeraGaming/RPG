df9a7d6ae9290ea75b0a981053ea1e689c9c2518
cbenjudah <97Turok@Gmail.com>
2021-06-29 21:13:05 -0800

    Version 0.0.1.2.1
    > Check Updates

--- RPG.py
+++ RPG.py
@@ -7,10 +7,11 @@
 # Imports
 import sys, time, os, random, pickle, items
 import os.path
+
 # Check if the file to save has been created.
 file_exists = os.path.isfile("loadfile.txt")
 
-# Load the text file
+# Load the text file (Bring down to save below us once working ...)
 def load():
     if file_exists:
         file = open("loadfile.txt", "r")
@@ -19,33 +20,19 @@ def load():
     else:
         room = "Start Room"
 
-    roomchooser(room)
+#===================================================================#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#===================================================================#
 
 # Save the file
 def save(s):
     file = open ("loadfile.txt", "w")
     file.write(s)
     file.close()
-    
-'''
-# Set the correct starting room
-def Tutorial(room):
-    if room == "Tutorial":
-        startroom()
 
-
-def startroom():
-    s = "Tuturial"
-    save(s)
-    print("You are in a hallway, you could go left or right"
-    direction = input()
-    if direction == "right":
-        secondroom()
-    quit()
-'''
-
-
-
+#===================================================================#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#===================================================================#
 
 # Login
 print(f'\n{"-"*36}\nWelcome to the Text RPG login screen\n{"-"*36}\n') 
@@ -73,8 +60,23 @@ while count < 3:
     if count == 3:
         exit()
 
+#===================================================================#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#===================================================================#
+
+############################
+##                        ##
+##         Welcome        ##
+##                        ##
+############################
+
+
 print (f'WELCOME {username_create} TO TEXT RPG V 1.1\n') 
 os.system("start F:\Python\Adventure.mp3")
+
+#===================================================================#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#===================================================================#
 
 class Weapon:
     # A simple class
@@ -94,28 +96,9 @@ class Weapon:
 # Object instantiation
 weapon = Weapon()
 
-
-# Slow Text Reference
-'''
-typing_speed = 50 #wpm
-def slow_type(t):
-    for l in t:
-        sys.stdout.write(l)
-        sys.stdout.flush()
-        time.sleep(random.random()*10.0/typing_speed)
-    print ('')
-'''
-
-# Sprint Text Reference
-'''
-def sprint(t):
-    for c in str + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(3./90)
-    print('')
-sprint()
-'''
+#===================================================================#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#===================================================================#
 
 ############################
 ##                        ##
@@ -130,32 +113,87 @@ sprint()
 # Story 2
 # Story 3
 
+Levels = ['Tutorial, Main Story, Veillantif']
+Tutorial = "Tutorial"
+Story_1 = "Phobotane"
+Story_2 = "Veillantif"
 
-# Start Gap
-print(f'{"-"*36}\nPrologue\n{"-"*36}\n')
+# Typing Speeds
 
-# Actual Start
 typing_speed = 50 #wpm
-def slow_type(text):
+def slow(text):
     for letter in text:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(random.random()*10.0/typing_speed)
     print('')
 
-slow_type(f'''Phobotane is a world of peace and prosperity. It is a 
-land in which all races live together in harmony. 
-The gnomes build their machines and live among
-the humans. Elves run the academies in which
-people come to learn. Ogres have no qualms
-living in the same town as humans or gnomes.
-Yet, all is not well.
+def normal(text):
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(random.random()*6.0/typing_speed)
+    print('')
 
-You are {username_create}, a human alchemist in the
-kingdom of Kuzos. You've lived in the town of
-Gerzona your entire life. You've been training in
-the local alchemist guild and have come to be a 
-respected member of the guild. The town is small,
-but it's enough to keep you busy. The town is
-surrounded by a wall to protect it from enemy
-attack. ''')
+def sprint(text):
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(random.random()*3.0/typing_speed)
+    print('')
+
+#===================================================================#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+#===================================================================#
+
+Game_Start = input(f"Please choose a Level - {Levels}: ")
+print('\n')
+while True:
+    if Game_Start == Tutorial:
+        slow(f'''
+Welcome to the Tutorial!
+Here you will understand the in and out of the game.''')
+        break
+
+    # Start Gap
+    if Game_Start == Story_1:
+        print(f'{"-"*36}\nPrologue\n{"-"*36}\n')
+
+        slow(f'''
+        Phobotane is a world of peace and prosperity. It is a 
+        land in which all races live together in harmony. 
+        The gnomes build their machines and live among
+        the humans. Elves run the academies in which
+        people come to learn. Ogres have no qualms
+        living in the same town as humans or gnomes.
+        Yet, all is not well.
+
+        You are {username_create}, a human alchemist in the
+        kingdom of Kuzos. You've lived in the town of
+        Gerzona your entire life. You've been training in
+        the local alchemist guild and have come to be a 
+        respected member of the guild. The town is small,
+        but it's enough to keep you busy. The town is
+        surrounded by a wall to protect it from enemy
+        attack. ''')
+        break
+    
+    if Game_Start == Story_2:
+        print(f'{"-"*36}\nPrologue\n{"-"*36}\n')
+
+        slow(f'''
+For Zelani the Dragon, the ancient world of Veillantif is a 
+dangerous one. Her friends, Marque the Cavernstrike and Sinon 
+the Werewolf, fair no better. Creatures like them are hunted 
+ceaselessly, leaving orphans and ghost towns in the human's wake.
+
+Like her father, she wishes to change the human's mind about 
+Varelsians. To fix what went wrong in the past. Though, it's tough 
+when humans only see you as a blood bag.
+''')
+        normal(f'''
+In eras past,testing speed testing speed testing speed...''')
+        break
+
+    else:
+        print("Incorrect Selection")
