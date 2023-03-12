5ea9dd421fdf4428a526ecade4277c903eb6db07
ChimeraGaming <54367698+ChimeraGaming@users.noreply.github.com>
2023-03-12 10:42:51 -0800

    Redid Game Code Completely

--- RPG_2.py
+++ RPG_2.py
@@ -1,134 +1,61 @@
 import random
+import time
 
-# Define the player's initial stats
-player_stats = {
-    "name": "Player",
-    "health": 100,
-    "attack": 10,
-    "defense": 5,
-    "level": 1,
-    "experience": 0,
-    "gold": 0,
-    "inventory": []
-}
+# Define adventurer stats and backstory
+adventurer = {'name': 'Arin', 'health': 100, 'attack': 10, 'defense': 5,
+              'backstory': "In the ancient world, there was a belief that the gods occasionally descended from their heavenly abode to walk among mortals and bestow blessings upon them. \n\nOne such descendant was a young adventurer named Arin. Arin's lineage could be traced back to one of the most powerful deities in the pantheon. \n\nHis great-great-great-grandmother was said to have been impregnated by the god himself, and from that union, Arin's family was born. \n\nFrom a young age, Arin showed signs of his divine heritage. He was faster, stronger, and more agile than the other children in his village. He also had an uncanny ability to sense danger and a remarkable talent for strategy and planning. \n\nAs he grew older, Arin became restless. He knew that he was destined for greatness and was determined to prove himself worthy of his divine heritage. He left his village and set out on a journey to explore the world and make a name for himself.\n"}
 
-# Define the boss monster's stats
-boss_stats = {
-    "name": "Boss",
-    "health": 200,
-    "attack": 20,
-    "defense": 10
-}
+# Define enemy stats
+enemy = {'name': 'Goblin', 'health': 50, 'attack': 5, 'defense': 2}
 
-# Define the different enemy types
-enemy_types = {
-    "Goblin": {"health": 20, "attack": 5, "defense": 2},
-    "Skeleton": {"health": 30, "attack": 10, "defense": 5},
-    "Orc": {"health": 40, "attack": 15, "defense": 8},
-    "Dragon": {"health": 100, "attack": 25, "defense": 15}
-}
-
-# Define the different items
-item_types = {
-    "Potion": {"health": 20},
-    "Sword": {"attack": 5},
-    "Shield": {"defense": 5}
-}
-
-# Define the different levels and the enemies that appear on each level
-levels = {
-    1: ["Goblin", "Skeleton"],
-    2: ["Orc", "Skeleton"],
-    3: ["Dragon", "Orc"]
-}
-
-# Define a function to display the player's stats
-def display_stats(stats):
-    print("Name:", stats["name"])
-    print("Level:", stats["level"])
-    print("Health:", stats["health"])
-    print("Attack:", stats["attack"])
-    print("Defense:", stats["defense"])
-    print("Experience:", stats["experience"])
-    print("Gold:", stats["gold"])
-    print("Inventory:", stats["inventory"])
-
-# Define a function to fight an enemy
-def fight_enemy(enemy_type):
-    enemy_stats = enemy_types[enemy_type]
-    print("You are fighting a", enemy_type)
-    while player_stats["health"] > 0 and enemy_stats["health"] > 0:
-        # Player attacks first
-        enemy_stats["health"] -= player_stats["attack"] - enemy_stats["defense"]
-        if enemy_stats["health"] <= 0:
-            print("You defeated the", enemy_type)
-            player_stats["experience"] += 10
-            player_stats["gold"] += 5
-            # Chance to drop an item
-            if random.randint(1, 10) <= 3:
-                item_type = random.choice(list(item_types.keys()))
-                item_stats = item_types[item_type]
-                player_stats["inventory"].append(item_type)
-                for stat, value in item_stats.items():
-                    player_stats[stat] += value
-                print("The", enemy_type, "dropped a", item_type)
+# Define a function to simulate a battle between adventurer and enemy
+def battle(adventurer, enemy):
+    backstory_lines = adventurer['backstory'].split('\n\n')
+    for line in backstory_lines:
+        print(line)
+        time.sleep(2)
+    
+    print('A', enemy['name'], 'has appeared!')
+    time.sleep(2)
+    
+    # Loop until either adventurer or enemy's health reaches zero
+    while adventurer['health'] > 0 and enemy['health'] > 0:
+        # Calculate adventurer's attack and enemy's defense
+        adventurer_attack = random.randint(1, adventurer['attack'])
+        enemy_defense = random.randint(1, enemy['defense'])
+        damage = adventurer_attack - enemy_defense
+        
+        # If adventurer's attack is successful, subtract damage from enemy's health
+        if damage > 0:
+            enemy['health'] -= damage
+            print(adventurer['name'], 'hits', enemy['name'], 'for', damage, 'damage!')
+            time.sleep(2)
+        
+        # If enemy's health reaches zero, adventurer wins
+        if enemy['health'] <= 0:
+            print('\n', adventurer['name'], 'defeats', enemy['name'], 'and wins the battle!')
+            time.sleep(2)
             break
-        # Enemy attacks next
-        player_stats["health"] -= enemy_stats["attack"] - player_stats["defense"]
-        if player_stats["health"] <= 0:
-            print("You were defeated by the", enemy_type)
-            return False
-    return True
+        
+        # Calculate enemy's attack and adventurer's defense
+        enemy_attack = random.randint(1, enemy['attack'])
+        adventurer_defense = random.randint(1, adventurer['defense'])
+        damage = enemy_attack - adventurer_defense
+        
+        # If enemy's attack is successful, subtract damage from adventurer's health
+        if damage > 0:
+            adventurer['health'] -= damage
+            print(enemy['name'], 'hits', adventurer['name'], 'for', damage, 'damage!')
+            time.sleep(2)
+        
+        # If adventurer's health reaches zero, enemy wins
+        if adventurer['health'] <= 0:
+            print('\n', enemy['name'], 'defeats', adventurer['name'], 'and wins the battle!\n\n')
+            time.sleep(2)
+            break
 
-# Define a function to display the items in the player's inventory
-def display_inventory():
-    print("Inventory:")
-    for item in player_stats["inventory"]:
-        print(item)
+# Call the battle function
+battle(adventurer, enemy)
 
-# Define the game loop
-while player_stats["health"] > 0 and boss_stats["health"] > 0:
-    # Check if the player has reached the boss level
-    if player_stats["level"] == len(levels):
-        print("You have reached the final level!")
-        print("Prepare to face the boss", boss_stats["name"])
-        input("Press Enter to continue...")
-        # Fight the boss
-        while player_stats["health"] > 0 and boss_stats["health"] > 0:
-            # Player attacks first
-            boss_stats["health"] -= player_stats["attack"] - boss_stats["defense"]
-            if boss_stats["health"] <= 0:
-                print("Congratulations! You have defeated the boss and completed the game!")
-                break
-            # Boss attacks next
-            player_stats["health"] -= boss_stats["attack"] - player_stats["defense"]
-            if player_stats["health"] <= 0:
-                print("You were defeated by the boss")
-                break
-    else:
-        # Get the list of enemies for the current level
-        level_enemies = levels[player_stats["level"]]
-        # Fight each enemy in the level
-        for enemy in level_enemies:
-            if not fight_enemy(enemy):
-                # Player was defeated
-                break
-        else:
-            # Player defeated all enemies in the level
-            print("You have completed level", player_stats["level"])
-            player_stats["level"] += 1
-            player_stats["experience"] += 20
-            player_stats["gold"] += 10
-            # Chance to get a random item
-            if random.randint(1, 10) <= 5:
-                item_type = random.choice(list(item_types.keys()))
-                item_stats = item_types[item_type]
-                player_stats["inventory"].append(item_type)
-                for stat, value in item_stats.items():
-                    player_stats[stat] += value
-                print("You found a", item_type)
-    # Display the player's stats and inventory
-    display_stats(player_stats)
-    display_inventory()
-    input("Press Enter to continue...")
-
+# Print
+print()
